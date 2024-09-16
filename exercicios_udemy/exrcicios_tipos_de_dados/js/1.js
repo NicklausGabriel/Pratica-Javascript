7812fa55dec198882ded0a90e67bfce6ea4a624c
Gabriel Avelar <gabriel.avelar@parcelaexpress.com.br>
2024-09-16 10:36:47 -0300

    add exercicio 1 tipos de dados string

---
+++ exercicios_udemy/exrcicios_tipos_de_dados/js/1.js
@@ -0,0 +1,13 @@
+/* 
+Escreva tres valores script e exiba no navegador com console.log(), com numeros inteiros, numeros com casas decimais e por aritimetica.
+ */
+let string1 = "batata";
+let string2 = "tenada";
+let strind3= "losbo";
+
+console.log("Ola "+ string1);
+console.log("Ola "+ string2);
+console.log(`Ola ${string1}`);
+
+
+
