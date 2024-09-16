fc075c7bf8805560917f996f239819de7d9939d3
Gabriel Avelar <gabriel.avelar@parcelaexpress.com.br>
2024-09-16 10:50:37 -0300

    exercicio 1 e 2, tipos de dados, string e numbers

---
+++ exercicios_udemy/exrcicios_tipos_de_dados/js/1.js
@@ -0,0 +1,10 @@
+/* 
+Escreva tres valores em string e exiba no navegador com console.log(); Com aspas duplas, aspas simples e templates literalss.
+ */
+let valor1 = "joab";
+let valor2 = "Jurema";
+let valor3 = "Joaboa";
+
+console.log("Ola "+ valor1);
+console.log('Ola '+ valor2);
+console.log(`Ola  ${valor3}`);
